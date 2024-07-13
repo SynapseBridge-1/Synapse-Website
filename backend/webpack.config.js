70d4d38fac369667987298465a477a1dcc811561
Talha-Dogar <dogartalha299@gmail.com>
2024-07-13 12:27:50 +0500

    updated configuration files

---
+++ backend/webpack.config.js
@@ -0,0 +1,29 @@
+import path from "path"; // Use import for ES6
+
+export default {
+  entry: './src/index.js', // Your entry file
+  target: 'node',
+  mode: 'production',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'index.js',
+    libraryTarget: 'commonjs2', // Ensures output is compatible with Node.js
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
+        },
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.js'],
+  },
+};
